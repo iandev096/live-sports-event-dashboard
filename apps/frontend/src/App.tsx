350d5a00cf70af299ef74a7acd332be0ffef364e
Isaac Anyimiah <iandev096@gmail.com>
2025-10-02 08:56:53 +0000

    feat: integrate SocketProvider and SimulationProvider into App component
    - Wrapped the Dashboard component with SocketProvider and SimulationProvider to manage socket connections and simulation state.
    - Enhanced the application structure for better context management and real-time updates.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,11 +1,16 @@
-import "./App.css";
 import Dashboard from "./components/composite/dashboard";
+import { SimulationProvider } from "./components/providers/simulation-provider";
+import { SocketProvider } from "./components/providers/socket-provider";
 import { ThemeProvider } from "./components/providers/theme-provider";
 
 function App() {
   return (
     <ThemeProvider>
-      <Dashboard />
+      <SocketProvider>
+        <SimulationProvider>
+          <Dashboard />
+        </SimulationProvider>
+      </SocketProvider>
     </ThemeProvider>
   );
 }
